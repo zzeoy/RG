eab49cbd59f260d8210b30d5c865f86fbcfcc2aa
zzeoy <2591181012@qq.com>
2021-04-16 14:42:05 +0800

    refactor homepage view to use a template

--- 1.py
+++ 1.py
@@ -1,4 +1,42 @@
 from selenium import webdriver
-browser=webdriver.Firefox()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.title
+from selenium.webdriver.common.keys import Keys
+import time
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser=webdriver.Firefox()
+        
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes
+        # to check out its homepage
+        self.browser.get('http://localhost:8000')
+        #She notices the page title and header mention to-do lists
+        self.assertIn('To-Do',self.browser.title)
+        header_text=self.browser.find_element_by_tag_name('hi').text
+        self.assertIn('To-Do',header_text)
+        #She is invited to enter a to-do item straight away
+        inputbox =self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+            )
+        inputbox.senf_keys(Keys.ENTER)
+        time.sleep(1)
+        table=self.browser.find_element_by_id('id_list_table')
+        rows=find_element_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text=='1:Buy peacock feathers' for row in rows).
+            "New to-do item did not apear in table"
+            )
+        
+        
+        self.fail('finish the test!')
+if __name__=='__main__':
+    unittest.main(warnings='ignore')
+
