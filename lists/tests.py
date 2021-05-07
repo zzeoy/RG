f916ee5ae2fc95d0242525599891e952f1a7435f
zzeoy <2591181012@qq.com>
2021-05-07 13:31:30 +0800

    Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -3,18 +3,20 @@ from django.urls import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from lists.views import home_page
+from django.template.loader import render_to_string
 
 class HomePageTest(TestCase):
     def test_root_url_resolve_to_home_page_view(self):
         found =resolve('/')
         self.assertEqual(found.func,home_page)
-    def test_home_page_return_correct_html(self):
-        request =HttpRequest()
-        response =home_page(request)
-        html=response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>',html)
-        self.assertTrue(html.endswith('</html>'))
+    def test_uses_home_template(self):
+        response=self.client.get('/')
+        self.assertTemplateUsed(response,'home.html')
+    def test_can_save_a_POST_request(self):
+        response=self.client.post('/',data={'item_text':'A new list item'})
+        self.assertIn('A new list item',response.content.decode())
+        self.assertTemplateUsed(response,'home.html')
+        
 
 # Create your tests here.
 #class SmokeTest(TestCase):
