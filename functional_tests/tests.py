a04469d971bc822cfd496b9e9ed374dbd9197858
zzeoy <2591181012@qq.com>
2021-05-21 14:31:55 +0800

    ok

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,10 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
-
+MAX_WAIT=10
 
 class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
@@ -13,12 +14,51 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
  
-
-    def check_for_row_in_list_table(self, row_text):
+    def test_can_start_a_for_one_user(self):
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+    def test_mutiple_user_can_start_lists_at_different_urls(self):
+        self.browser.get(self.live_server_url)
+        inputbox =self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)               
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+        
+        edith_list_url=self.browser.current_url
+        self.assertRegex(edith_list_url,'/lists/.+')
+        
+        self.browser.quit()
+        self.browser=webdriver.Firefox()
+        
+        self.browser.find_element_by_tag_name('body').test
+        self.assertNotIn('Buy peacock feathers',page_text)
+        self.assertNotIn('make a fly',page_text)
+        
+        inputbox =self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)               
+        self.wait_for_row_in_list_table('1: Buy milk')
+        
+        francis_list_url=self.browser.current_url
+        self.assertRegex(francis_list_url,'/lists/.+')
+        self.assertNotEqual(francis_list_url,edith_list_url)
+        
+        page_text=self.browser.find_element_by_tag_name('body').text
+        self.assertIn('Buy milk',page_text)
+        
+    def wait_for_row_in_list_table(self, row_text):
         """辅助方法:检查row.text"""
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+        start_time=time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except(AssertionError,WebDriverException) as e:
+                if time.time()-start_time>MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
 
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -39,31 +79,16 @@ class NewVisitorTest(LiveServerTestCase):
             
         inputbox.send_keys('Buy peacock feathers')
         # 回车键
-        inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)               
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # 表格中数据有变化时，页面会自动刷新，需要重新查找元素
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
-        '''
-        self.assertTrue(
-            any(row.text=='1:Buy peacock feathers' for row in rows),
-            f"New to-do item did not apear in table.Contents were:\n{table.text}"
-            )
-        
-        self .assertIn('1:Buy peacock feathers',[row.text for row in rows])
-        self.assertIn(
-        '2:Use peacock feathers to make a fly',
-        [row.text for row in rows]
-        )
-        '''
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
         
         self.fail('finish the test!')
 #if __name__=='__main__':
--- superlists/urls.py
+++ superlists/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from django.conf.urls import url
+from django.urls import path,re_path
 from lists import views
 urlpatterns=[
-    url(r'^$',views.home_page,name='home'),
+    path('admin/',admin.site.urls),
+    re_path(r'^$',views.home_page,name='home'),
     ]
